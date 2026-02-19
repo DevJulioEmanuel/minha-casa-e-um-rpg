b0aba34f1290c0a96ce9f6d4ec1d6d13ebdfcaa7
Arthur32p <arthurlelisuchoa32@gmail.com>
2026-02-19 14:12:20 -0300

    Adicionando estrutura básica do projeto spring boot

---
+++ backend/src/main/java/com/repquest/api/RepublicaQuestApiApplication.java
@@ -0,0 +1,13 @@
+package com.repquest.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepublicaQuestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepublicaQuestApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/repquest/api/RepublicaQuestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.repquest.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepublicaQuestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
